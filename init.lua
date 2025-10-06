3399fad748dd8ba3a052f0fc627a00dd32ed88d4
Jakub Kluwa <jkluwa4@gmail.com>
2025-10-06 22:08:06 +0200

    c++ debbuging

--- init.lua
+++ init.lua
@@ -715,6 +715,7 @@ require('lazy').setup({
         'stylua', -- Used to format Lua code
         'clangd',
         'clang-format',
+        'codelldb',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -13,7 +13,12 @@ return {
 
       require('nvim-dap-virtual-text').setup()
       dapui.setup()
-
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
       dap.listeners.after.event_initialized['dapui_config'] = function()
         dapui.open()
       end
